22696bdecacb5b58ed6cab6ac4b432c195259ac6
KamiStachowicz <75569396+KamiStachowicz@users.noreply.github.com>
2023-05-07 12:10:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ This module contains a Flask application that displays a greeting message.
 """
 import sys
 
+from flask import Flask
+
+app = Flask(__name__)
+
 def print_python_version():
     """Prints the current Python version."""
     print(sys.version)
 
-
-from flask import Flask
-
-app = Flask(__name__)
 
 @app.route('/')
 def index():
