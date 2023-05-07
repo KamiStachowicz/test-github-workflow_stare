645714051b6c375a1107b9ebaa784df1f0f17a8a
KamiStachowicz <75569396+KamiStachowicz@users.noreply.github.com>
2023-05-07 12:12:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """
 This module contains a Flask application that displays a greeting message.
 """
-import sys
 
+import sys
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,11 +11,9 @@ def print_python_version():
     """Prints the current Python version."""
     print(sys.version)
 
-
 @app.route('/')
 def index():
     return '<h1>Hello World! Greetings from Flask!</h1>'
-
 
 if __name__ == "__main__":
     app.run(debug=True)
