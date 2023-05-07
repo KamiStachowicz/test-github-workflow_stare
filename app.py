af4c0467217120d1bf2b71f369edad7744dcb773
KamiStachowicz <75569396+KamiStachowicz@users.noreply.github.com>
2023-05-07 11:47:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+"""
+This module contains a Flask application that displays a greeting message.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Render the home page."""
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    return '<h1>Hello World! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
